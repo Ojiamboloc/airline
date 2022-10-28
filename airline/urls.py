8c1740d2923cda74b7b41fa169e9c48707077a4c
Ojambo Felix <ojiamboloc@gmail.com>
2022-10-28 09:26:22 +0300

    Authentiication added

--- airline/urls.py
+++ airline/urls.py
@@ -17,8 +17,9 @@ from django.contrib import admin
 from django.urls import include,path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("",include("flights.urls")),
+    path('', admin.site.urls),
+    path("flights/",include("flights.urls")),
+    path("users", include("users.urls"))
     # path('',include('flights.urls'))
 ]
 
